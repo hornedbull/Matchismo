447c632751801597a2ffcfba056c7b9e0e38a958
Vatsal Shah <v536306@C02GQ3QKDV7N.local>
2013-10-24 15:57:18 -0400

    Initial Commit

---
+++ Matchismo/CardGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  CardGameViewController.h
+//  Matchismo
+//
+//  Created by Shah, Vatsal on 10/24/13.
+//  Copyright (c) 2013 Vatsal's Factory. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameViewController : UIViewController
+
+@end
---
+++ Matchismo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Matchismo
+//
+//  Created by Shah, Vatsal on 10/24/13.
+//  Copyright (c) 2013 Vatsal's Factory. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CardGameAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CardGameAppDelegate class]));
+    }
+}
